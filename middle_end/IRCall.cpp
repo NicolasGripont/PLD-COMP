2e37b563976082a816ea38728732b91ca7c51c61
Quentin Vecchio <vecchioquentin@hotmail.fr>
2017-03-28 20:31:46 +0200

    Add code in to IRCall

--- middle_end/IRCall.cpp
+++ middle_end/IRCall.cpp
@@ -1,7 +1,30 @@
 #include "IRCall.h"
 
-IRCall::IRCall()
+IRCall::IRCall(BasicBlock* _block, std::string _name, Symbol *_returnSymbol) : IRInstruction(_block, IR_Operation.CALL), name(_name), returnSymbol(_returnSymbol)
 {
 
 }
 
+IRCall::~IRCall() {
+	delete returnSymbol;
+	for( auto s : params) {
+		delete s;
+	}
+}
+    
+void IRCall::addParameter(Symbol * symbol) {
+	if(symbol != nullptr) {
+		params.push_back(symbol);
+	}
+}
+
+std::string IRCall::toString() const {
+	std::string paramsStr = "";
+	for( auto s : params) {
+		if(s != nullptr) {
+			paramsStr += s->getName() + ", ";
+		}
+	}
+	std::string res = "call " + name + "(" + + ")";
+	return res;
+}
--- middle_end/IRCall.h
+++ middle_end/IRCall.h
@@ -1,12 +1,22 @@
 #ifndef IRCALL_H
 #define IRCALL_H
-
+#include <iostream>
+#include <vector>
+#include "BasicBlock.h"
+#include "Symbol"
 
 class IRCall
 {
 public:
-    IRCall();
+    IRCall(BasicBlock* block, std::string name, Symbol *returnSymbol);
+    ~IRCall();
+    void addParameter(Symbol * symbol);
+    std::string toString() const;
 
+protected:
+	std::string name;
+	Symbol *returnSymbol;
+	vector<Symbol *> params;
 };
 
 #endif // IRCALL_H
