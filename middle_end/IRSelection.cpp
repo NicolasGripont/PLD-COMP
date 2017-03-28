1775b4fa420999bc50a6b2ecf14bd37b72bad414
Quentin Vecchio <vecchioquentin@hotmail.fr>
2017-03-28 20:22:15 +0200

    Add code into IRSelection

--- middle_end/IRSelection.cpp
+++ middle_end/IRSelection.cpp
@@ -1,7 +1,22 @@
 #include "IRSelection.h"
-
-IRSelection::IRSelection()
+ 
+IRSelection::IRSelection(BasicBlock* _block, Symbol* _condition, string _ifLabel, string _elseLabel) : IRInstruction(block, IR_Operation.SELECTION), 
+																										condition(_condition), ifLabel(_ifLabel), elseLabel(_elseLabel) 
 {
 
 }
 
+IRSelection::~IRSelection() {
+	delete condition;
+}
+    
+std::string IRSelection::toString() const {
+	std::string cond = "";
+	if(condition != nullptr) {
+		cond = condition->getName();
+	}
+	std::string res = "if " + cond + " then " + label1;
+	if(labelElse != LABEL_NULL_NAME) {
+		res += " else " + label2;
+	}
+}
--- middle_end/IRSelection.h
+++ middle_end/IRSelection.h
@@ -1,12 +1,20 @@
 #ifndef IRSELECTION_H
 #define IRSELECTION_H
+#include <iostream>
+#include "IRInsctruction.h"
 
-
-class IRSelection
+class IRSelection : IRInsctruction
 {
 public:
-    IRSelection();
+	static const string LABEL_NULL_NAME = "null";
+    IRSelection(BasicBlock* block, Symbol* condition, string ifLabel, string elseLabel = LABEL_NULL_NAME);
+    ~IRSelection();
+    std::string toString() const;
 
+protected:
+	Symbol *condition;
+	string ifLabel;
+	string elseLabel;
 };
 
 #endif // IRSELECTION_H
