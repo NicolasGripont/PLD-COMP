6bb5eb8bd5e40ffe0b6380f767b7be20ec974273
NicolasGripont <Nicolas.Gripont@insa-lyon.fr>
2017-04-07 17:18:20 +0200

    Motif touchdown.c et alphabet.c

--- tests/alphabet.c
+++ tests/alphabet.c
@@ -3,22 +3,22 @@
 
 void print_alphabet_recurcive(char c);
 
-//void print_alphabet_while()
-//{
-//	char c = 'a';
-//	char end = 'z' + 1;
-//
-//	while (c != end)
-//	{
-//		putchar(c);
-//		c++;
-//	}
-//	putchar('\n');
-//}
+void print_alphabet_while()
+{
+    char c = 'a';
+    char end = 'z' + 1;
+
+    while (c != end)
+    {
+        putchar(c);
+        c++;
+    }
+    putchar('\n');
+}
 
 int64_t main() 
 {
-//	print_alphabet_while();
+    print_alphabet_while();
 	print_alphabet_recurcive('a');
 	return 0;
 }
@@ -29,7 +29,7 @@ void print_alphabet_recurcive(char c)
 	putchar(c);
 	if(c == 'z') {
 		putchar('\n');
-		return;
-	}
-	print_alphabet_recurcive(++c);
+    } else {
+        print_alphabet_recurcive(++c);
+    }
 }
--- tests/touchdown.c
+++ tests/touchdown.c
@@ -2,43 +2,51 @@
 #include "stdio.h"
 
 int64_t factoriel_while(int64_t number);
+void print_recursive1(int64_t number);
+void print_recursive2(int64_t number);
 void print(int64_t); 
 
-int64_t factoriel_recurcive(int64_t number) 
+int64_t factoriel_recurcive(int64_t number)
 {
-	int64_t result = 1;
-	int64_t i = 2; 
+    int64_t result = 1;
+    int64_t i = 2;
 
     if(number < 0) //impossible
-	{
-        return 0;
-	}
+    {
+        result = 0;
+    } else {
+        if(number < 2) {
+            result = 1;
+        } else {
+            result = number * factoriel_recurcive(number - 1);
+        }
+    }
 
-	if(number < 2) {
-		return 1;
-	}
 
-	return number * factoriel_recurcive(number - 1);
+    return result;
 }
 
 
 int64_t main () 
 {
-	int64_t factoriel1;
+	int64_t factoriel;
     int64_t i = 0;
 	
-	for( ; i < 6 ; i++)
-	{
-		factoriel1	= factoriel_while(i);
-		print(factoriel1);
-	}
+    while(i < 6)
+    {
+        factoriel = factoriel_while(i);
+        // print(factoriel1);
+//        print_recursive1(factoriel);
+        i++;
+    }
 
-	i = 0;
-	for( ; i < 6 ; i++)
-	{
-		factoriel1	= factoriel_recurcive(i);
-		print(factoriel1);
-	}
+    i = 0;
+    while(i < 6)
+    {
+        factoriel = factoriel_recurcive(i);
+//        print(factoriel);
+        i++;
+    }
 	putchar('T');
 	putchar('O');
 	putchar('U');
@@ -51,58 +59,83 @@ int64_t main ()
     putchar(' ');
     putchar('!');
 	putchar('\n');
-	
+
 	return 0;
 }
 
 
-int64_t factoriel_while(int64_t number) 
+int64_t factoriel_while(int64_t number)
 {
-	int64_t result = 1;
-	int64_t i = 2; 
+    int64_t result = 1;
+    int64_t i = 2;
 
     if(number < 0) //impossible
-	{
-        return 0;
-	}
+    {
+        result = 0;
+    }
 
-	while(i <= number) 
-	{
-		result *= i;
-		i++;
-	}
+    while(i <= number)
+    {
+        result *= i;
+        i++;
+    }
 
-	return result;
+    return result;
 }
 
 void print(int64_t result)
 {
     char sign = '+';
-	int64_t reverse = 0;
-	int64_t i = 1;
+    int64_t reverse = 0;
+    int64_t i = 1;
     char base = '0';
-	char currentChar;
-	int64_t currentDigit;
+    char currentChar;
+    int64_t currentDigit;
 
-	if(result < 0) 
-	{
-		result *= -1; 
+    if(result < 0)
+    {
+        result *= -1;
         putchar('-');
-	} 
+    }
 
-	while((result / i) > 10) 
-	{
-		i *= 10;
-	}
+    while((result / i) > 10)
+    {
+        i *= 10;
+    }
 
-	while (i > 0) 
-	{
-		currentDigit = result / i;
-		currentChar = base + currentDigit;
-		putchar(currentChar);
-		result %= i;
-		i /= 10;
-	}	
+    while (i > 0)
+    {
+        currentDigit = result / i;
+        currentChar = base + currentDigit;
+        putchar(currentChar);
+        result %= i;
+        i /= 10;
+    }
 
-	putchar('\n');
+    putchar('\n');
+}
+
+void print_recursive1(int64_t number)
+{
+    if(number < 0)
+    {
+        number *= -1;
+        putchar('-');
+    }
+    if(number == 0) {
+        putchar('0');
+    } else {
+        print_recursive2(number);
+    }
+
+    putchar('\n');
+}
+
+
+void print_recursive2(int64_t number)
+{
+    if(number != 0) {
+        print_recursive2(number/10);
+        putchar('0'+number%10);
+    }
 }
