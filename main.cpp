df35a16d5e8edf6b8f1d34fd49efff48bc5f0405
Arnaud <arnaudfavier@users.noreply.github.com>
2017-04-09 22:30:20 +0200

    Clean repo

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@
 
 int main(int argc, char* argv[])
 {
-    std::cout << "-> Print du programme \n" << std::endl;
+    std::cout << "-> Print du programme\n" << std::endl;
     /* Bison */
     Genesis* genesis = bison(argc, argv);
 
@@ -22,27 +22,31 @@ int main(int argc, char* argv[])
     Parser astToIRParser;
     astToIRParser.generateIR(genesis);
 
-    std::cout << "\n-> Print de l'IR \n" << std::endl;
+    std::cout << "\n-> Print de l'IR\n" << std::endl;
     std::cout << "NB CFG : " << astToIRParser.getFunctionCFG().size() << std::endl;
     for(auto pair : astToIRParser.getFunctionCFG())
     {
         std::cout << pair.second << std::endl;
     }
 
-    std::cout << "\n-> Assemblage \n" << std::endl;
+    std::cout << "\n-> Assemblage\n" << std::endl;
 	/* Backend x64 */
+    std::cout << "   - x64: starting..." << std::endl;
 	X64* x64 = new X64("x64", astToIRParser.getFunctionCFG());
 	x64->parse();
 	x64->compile();
 
     delete x64;
+    std::cout << "   - x64:         ...ending." << std::endl;
 
     /* Backend Java */
+    std::cout << "   - Java: starting..." << std::endl;
     Java* java = new Java("java", astToIRParser.getFunctionCFG());
     java->parse();
     java->compile();
 
 	delete java;
+    std::cout << "   - Java:         ...ending." << std::endl;
 
     if (genesis != nullptr)
     {
